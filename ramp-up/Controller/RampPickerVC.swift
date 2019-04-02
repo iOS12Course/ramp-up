abe6287f2646b5e692f61040a463ad3205f353c2
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-01 22:24:01 -0500

    Moving objects in 3D space in augmented reality

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -31,6 +31,8 @@ class RampPickerVC: UIViewController {
         view.insertSubview(sceneView, at: 0)
         
         preferredContentSize = size
+        view.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.layer.borderWidth = 3.0 
         
         let scene = SCNScene(named: "art.scnassets/ramps.scn")!
         sceneView.scene = scene
@@ -65,6 +67,7 @@ class RampPickerVC: UIViewController {
             let node = hitResults[0].node
             debugPrint(node.name!)
             rampPLacerVC.onRampSelected(node.name!)
+            dismiss(animated: true, completion: nil)
         }
         
     }
